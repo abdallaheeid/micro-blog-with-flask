fe31a2771cccd4dac20f37a2064864e3e3813a39
Abdallah Eid <abdallaheeid@icloud.com>
2025-11-06 17:13:48 +0100

    Profile Page and Avatar

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 @login.user_loader
 def load_user(id):
@@ -19,6 +20,9 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -31,6 +35,10 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
 
 class Post(db.Model):
